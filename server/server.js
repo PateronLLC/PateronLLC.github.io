586574d35c1ee245d76c935fbb7b7935a6862903
Cameron Simmons <cssim22@gmail.com>
2022-04-06 19:14:32 -0700

    resolved ecmascript module issues with old version of react and fixed bug relating to event listener triggers during animation.

--- server/server.js
+++ server/server.js
@@ -11,37 +11,38 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = 9000;
 
+console.log('Node Environment:', process.env.NODE_ENV);
 // app.use(helmet());
 app.use(express.json());
 // app.use(authRouter);
 app.use('/user', userRouter);
 
-app.use('/static', express.static(path.join(__dirname, '../build/static')));
+app.use('/', express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res, next) {
-  res.sendFile(path.resolve(__dirname, '../build/index.html'));
-});
+// app.get('/', function (req, res, next) {
+// 	res.sendFile(path.resolve(__dirname, '../build/index.html'));
+// });
 
 //catch all route handler for unknown routes
 app.use((req, res) => {
-  return res.status(404).send("Sorry, I can't find that endpoint.");
+	return res.status(404).send("Sorry, I can't find that endpoint.");
 });
 
 //global express error handler
 app.use((err, req, res) => {
-  const defaultError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occured' },
-  };
-  const errorObj = Object.assign({}, defaultError, err);
-  console.error(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+	const defaultError = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 500,
+		message: { err: 'An error occured' },
+	};
+	const errorObj = Object.assign({}, defaultError, err);
+	console.error(errorObj.log);
+	return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => {
-  // req
-  console.log(`Server listening on port ${PORT}`);
+	// req
+	console.log(`Server listening on port ${PORT}`);
 });
 
 export default app;
