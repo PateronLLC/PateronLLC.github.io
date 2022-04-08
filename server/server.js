fe5c24c5bbca37c491cd7253171743daa5f26cd3
Patrick Sullivan <patrick@jsullivan.org>
2022-04-07 19:24:58 -0700

    Refactored state setter functions as handlers, implemented modal state and toggle function as state machine design pattern, blocked user game input on any open modal, connected frontend fetch request to backend \"/user\" endpoint

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ console.log('Node Environment:', process.env.NODE_ENV);
 // app.use(helmet());
 app.use(express.json());
 // app.use(authRouter);
+// app.use((req, res) => {
+//   console.log('req.body', req.body);
+// });
 app.use('/user', userRouter);
 
 app.use('/', express.static(path.join(__dirname, '../build')));
@@ -25,24 +28,24 @@ app.use('/', express.static(path.join(__dirname, '../build')));
 
 //catch all route handler for unknown routes
 app.use((req, res) => {
-	return res.status(404).send("Sorry, I can't find that endpoint.");
+  return res.status(404).send("Sorry, I can't find that endpoint.");
 });
 
 //global express error handler
 app.use((err, req, res) => {
-	const defaultError = {
-		log: 'Express error handler caught unknown middleware error',
-		status: 500,
-		message: { err: 'An error occured' },
-	};
-	const errorObj = Object.assign({}, defaultError, err);
-	console.error(errorObj.log);
-	return res.status(errorObj.status).json(errorObj.message);
+  const defaultError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occured' },
+  };
+  const errorObj = Object.assign({}, defaultError, err);
+  console.error(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(PORT, () => {
-	// req
-	console.log(`Server listening on port ${PORT}`);
+  // req
+  console.log(`Server listening on port ${PORT}`);
 });
 
 export default app;
--- src/components/header/StatBar.js
+++ src/components/header/StatBar.js
@@ -13,41 +13,38 @@ import { DialogContentText } from '@mui/material';
 // }
 
 const StatItem = ({ label, value }) => {
-	return (
-		<div className="stat-item">
-			<DialogContentText
-				className="dialogtext"
-				variant="h6"
-				sx={{
-					fontWeight: 'bold',
-					fontSize: `calc(min(3vw, 14px))`,
-				}}
-			>
-				{label}
-			</DialogContentText>
-			<DialogContentText
-				className="dialogtext"
-				sx={{
-					fontSize: `calc(min(3vw, 14px))`,
-				}}
-			>
-				{value}
-			</DialogContentText>
-		</div>
-	);
+  return (
+    <div className="stat-item">
+      <DialogContentText
+        className="dialogtext"
+        variant="h6"
+        sx={{
+          fontWeight: 'bold',
+          fontSize: `calc(min(3vw, 14px))`,
+        }}
+      >
+        {label}
+      </DialogContentText>
+      <DialogContentText
+        className="dialogtext"
+        sx={{
+          fontSize: `calc(min(3vw, 14px))`,
+        }}
+      >
+        {value}
+      </DialogContentText>
+    </div>
+  );
 };
 
 export const StatBar = ({ stats }) => {
-	return (
-		<div className="stat-bar">
-			<StatItem label="Total Games" value={stats.totalGames} />
-			<StatItem label="Total Wins" value={`${stats.wins}`} />
-			<StatItem
-				label="Total losses"
-				value={`${stats.totalGames - stats.wins}`}
-			/>
-			<StatItem label="Current Streak" value={stats.currentStreak} />
-			<StatItem label="Best Streak" value={stats.bestStreak} />
-		</div>
-	);
+  return (
+    <div className="stat-bar">
+      <StatItem label="Total Games" value={stats.totalGames} />
+      <StatItem label="Total Wins" value={`${stats.wins}`} />
+      <StatItem label="Total losses" value={`${stats.totalGames - stats.wins}`} />
+      <StatItem label="Current Streak" value={stats.currentStreak} />
+      <StatItem label="Best Streak" value={stats.bestStreak} />
+    </div>
+  );
 };
